0499f5309afaf2edabb0afa6efd409a3c9a5e6ce
Nilay Yılmaz <nilay.yilmaz115@gmail.com>
2026-08-10 14:29:10 +0300

    Pytest fixture eklendi

--- test_app.py
+++ test_app.py
@@ -1,39 +1,42 @@
+import pytest
 from app import app
 
-def test_home():
-    client = app.test_client()
+@pytest.fixture
+def client():
+    return app.test_client()
 
+
+def test_home(client):
     response = client.get("/")
-
     assert response.status_code == 200
 
-def test_tasks_key_exists():
-    client = app.test_client()
+def test_tasks_key_exists(client):
     response = client.get("/")
-
     data = response.get_json()
 
     assert "tasks" in data
 
 
-def test_tasks_is_list():
-    client = app.test_client()
+def test_tasks_is_list(client):
     response = client.get("/")
-
     data = response.get_json()
 
     assert isinstance(data["tasks"], list)
 
-def test_tasks_not_empty():
-    client = app.test_client()
+def test_tasks_not_empty(client):
     response = client.get("/")
-
     data = response.get_json()
 
     assert len(data["tasks"]) >  0
 
-def test_not_found():
-    client = app.test_client()
+def test_not_found(client):
     response = client.get("/olmayan-sayfa")
 
     assert response.status_code == 404
+
+def test_tasks_are_strings(client):
+    response = client.get("/")
+    data = response.get_json()
+
+    for task in data["tasks"]:
+        assert isinstance(task, str)
