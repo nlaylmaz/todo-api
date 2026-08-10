9f9c44099f82397b5d1c17668955930d481246b0
Nilay Yılmaz <nilay.yilmaz115@gmail.com>
2026-08-10 09:33:16 +0300

    Test fail denemesi

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return jsonify({
-        "tasks": [
+        "liste": [
             "Docker ogren",
             "CI/CD ogren",
 	    "Azure deploy ogren"
